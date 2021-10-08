4dd752f0676c84bf54f763e47a905f676ef3a308
Alaxend0l <alaxend0l@gmail.com>
2021-10-08 17:41:40 -0500

    Added enemy shooting script

---
+++ Fall 2021 Game Jam/Assets/Scripts/Shooting.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public GameObject projectile;
+    public bool automatic;
+    public float projectilesPerShoot;
+    public float timeBetweenShoot;
+    private float timeBetweenShootCurrent;
+
+    void Start()
+    {
+        timeBetweenShootCurrent = timeBetweenShoot;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (automatic)
+        {
+            timeBetweenShootCurrent -= Time.deltaTime;
+            if (timeBetweenShootCurrent < 0)
+            {
+                timeBetweenShootCurrent = timeBetweenShoot;
+                Shoot();
+            }
+        }
+    }
+
+    void Shoot()
+    {
+        for (int i = 0; i < projectilesPerShoot; i++)
+        {
+            Instantiate(projectile, transform.position, transform.rotation);
+        }
+    }
+}
