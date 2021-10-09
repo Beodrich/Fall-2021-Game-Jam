b165af25e0497a9668b5199dc8ef363eac5bfa73
Alaxend0l <alaxend0l@gmail.com>
2021-10-08 19:58:32 -0500

    Added pizza health thing

---
+++ Fall 2021 Game Jam/Assets/Scripts/PizzaHealth.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PizzaHealth : MonoBehaviour
+{
+    public int pizzaSlices;
+    private Vector3 startingPosition;
+    public float shakingMagnitude;
+    public float shakingTime;
+    private float shakingTimeLeft;
+
+    void Start()
+    {
+        startingPosition = transform.position;
+        shakingTimeLeft = 5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (shakingTimeLeft > 0)
+        {
+            transform.position = startingPosition + new Vector3(Random.Range(-shakingMagnitude, shakingMagnitude), Random.Range(-shakingMagnitude, shakingMagnitude), 0);
+            shakingTimeLeft -= Time.deltaTime;
+        }
+        else
+        {
+            transform.position = startingPosition;
+        }
+    }
+
+    public void RemovePizzaSlices(int value)
+    {
+        shakingTimeLeft = shakingTime;
+    }
+
+
+}
