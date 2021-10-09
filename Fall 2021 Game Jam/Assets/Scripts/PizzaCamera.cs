41198422dc74685320c4c5646c332e473acd137c
Alaxend0l <alaxend0l@gmail.com>
2021-10-09 12:33:40 -0500

    Added camera lock in scene

--- Fall 2021 Game Jam/Assets/Scripts/PizzaCamera.cs
+++ Fall 2021 Game Jam/Assets/Scripts/PizzaCamera.cs
@@ -6,15 +6,24 @@ public class PizzaCamera : MonoBehaviour
 {
     public Transform cameraTransform;
     private Vector3 beginningOffset;
+    public Vector2 cameraBoundary;
+    private Camera thisCamera;
     // Start is called before the first frame update
     void Start()
     {
         beginningOffset = transform.position;
+        thisCamera = GetComponent<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = cameraTransform.position + beginningOffset;
+        //Vector3 newPos = cameraTransform.position + beginningOffset;
+        Vector3 newPos = cameraTransform.position + new Vector3(0,0,-10);
+        float halfHeight = thisCamera.orthographicSize;
+        float halfWidth = thisCamera.aspect * halfHeight;
+        newPos.x = Mathf.Clamp(newPos.x, halfWidth, cameraBoundary.x - halfWidth);
+        newPos.y = Mathf.Clamp(newPos.y, halfHeight, cameraBoundary.y - halfHeight);
+        transform.position = newPos;
     }
 }
