3bcd6c805359e488a173f2810132d6835b2f0079
Ziad-107 <60474322+Ziad-107@users.noreply.github.com>
2021-10-08 19:33:53 -0500

    Whack
    It can WHACK

---
+++ Fall 2021 Game Jam/Assets/Scripts/WhackScript.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhackScript : MonoBehaviour
+{
+    private float WhackLag; //No spam clicking
+    public float WhackLagStart; 
+    public Transform Whackpos; //Attack Position
+    public float WhackRadius; //Attack HitBox Radius
+
+    public LayerMask DefineEnemy;
+    public int damage; 
+
+       Vector3 mouse_pos;
+       Vector3 object_pos;
+       public Transform target;
+       public float angle; 
+ 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    { //objectPos.x
+
+mouse_pos = Input.mousePosition;
+     
+     
+     mouse_pos.z = 5; //The distance between the camera and object
+     object_pos = Camera.main.WorldToScreenPoint(target.position);
+     mouse_pos.x = mouse_pos.x - object_pos.x;
+     mouse_pos.y = mouse_pos.y - object_pos.y;
+     angle = Mathf.Atan2(mouse_pos.y, mouse_pos.x) * Mathf.Rad2Deg;
+     transform.rotation = Quaternion.Euler(0, 0, angle);
+     
+        
+        
+        if (WhackLag <= 0) // 
+        {
+            if (Input.GetMouseButton(0))
+            {
+           Collider2D[] CheckDamage = Physics2D.OverlapCircleAll(Whackpos.position, WhackRadius, DefineEnemy);
+           for (int i = 0; i<CheckDamage.Length; i++ )
+           {
+               CheckDamage[i].GetComponent<TV>().health -= damage;
+           }
+            }
+
+
+        }
+        else 
+        WhackLag -= Time.deltaTime; 
+    }
+
+    void OnDrawGizmosSelected()
+    {
+    Gizmos.color = Color.red;
+    Gizmos.DrawWireSphere(Whackpos.position, WhackRadius);
+
+    }
+
+}
