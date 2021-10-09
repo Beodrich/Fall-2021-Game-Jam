452b7ea1735aa9ea5de39d21c666b8f543fe8497
Beodrich <60353906+Beodrich@users.noreply.github.com>
2021-10-08 19:09:29 -0500

    added basic navmesh

---
+++ Fall 2021 Game Jam/Assets/Scripts/TV.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TV : MonoBehaviour
+{
+    public GameObject player;
+    private NavMeshAgent agent;
+
+    public int heath;
+    
+    private void Start() {
+        agent=GetComponent<NavMeshAgent>();
+        agent.updateRotation=false;
+        agent.updatePosition=false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(player.transform.position);
+    }
+    public void Die(){
+
+    }
+}
