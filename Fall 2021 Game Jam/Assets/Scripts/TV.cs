619930145cfdfd79c1933e4c4a90284d13c900cd
Alaxend0l <alaxend0l@gmail.com>
2021-10-09 10:44:20 -0500

    Added healthbar to enemies

--- Fall 2021 Game Jam/Assets/Scripts/TV.cs
+++ Fall 2021 Game Jam/Assets/Scripts/TV.cs
@@ -6,6 +6,7 @@ public class TV : MonoBehaviour
 {
     public PatrolPoint patrolPoint;
     public GameObject player;
+    public Healthbar healthbar;
 
     public int health;
 
@@ -49,6 +50,7 @@ public class TV : MonoBehaviour
 
     public void TakeDamage(){
         health=Mathf.Max(0,health-1);
+        healthbar.healthCur = health;
         if(health==0){
             Destroy(this.gameObject);
 
